35e63789496867989822bb144dc63877bcfdbbd4
Ben O'Neill <ben@oneill.sh>
2025-11-10 14:53:28 -0500

    Fix duplicate CFLAGS

--- config.mk
+++ config.mk
@@ -6,10 +6,8 @@ CC       = gcc
 LD       = $(CC)
 
 CFLAGS    = -std=c89 -pedantic -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 -D_FILE_OFFSET_BITS=64
+CFLAGS   += -DBF_VERSION="\"$(VERSION)\""
 CFLAGS   += -O3 -ffast-math
 CFLAGS   += -Wall -Wextra -Wpedantic -Werror
 CFLAGS   += -Wno-unused-parameter -Wno-unused-function -Wno-unused-variable -Wno-missing-field-initializers
-
-CPPFLAGS  = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 \
-           -D_FILE_OFFSET_BITS=64 -DBF_VERSION="\"$(VERSION)\""
-LDFLAGS   = -s
+CFLAGS   += -s
